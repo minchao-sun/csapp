e21ed5cf18769ae5f5e183767224485cd19e197b
Minchao Sun <minchao.sun@outlook.com>
2024-09-22 20:55:28 +0800

    lab3

--- start_docker.sh
+++ start_docker.sh
@@ -10,3 +10,24 @@ docker run -it \
 -v ".:/csapp" \
 --name=csapp \
 csapp_image /bin/bash
+
+# # in vm
+# apt-get install qemu-user-static
+
+# # debug one x86 binary
+# cd /root/bomb
+# qemu-x86_64-static -g 1234 ./bomb
+
+
+# # in another tty
+# gdb ./bomb
+# (gdb) target remote :1234
+# (gdb) b *0x400ee4
+# Breakpoint 1 at 0x400ee4
+# (gdb) continue
+# Continuing.
+
+# Breakpoint 1, 0x0000000000400ee4 in phase_1 ()
+# (gdb) x/s 0x402400
+# 0x402400:	"Border relations with Canada have never been better."
+# (gdb) quit
