b4ec3b265e1743dbe7abedc0a005b02f34ca8ca2
Minchao Sun <minchao.sun@outlook.com>
2024-09-23 23:26:41 +0800

    lab3 part1 level3

--- lab3-attack/sol.sh
+++ lab3-attack/sol.sh
@@ -1,7 +1,11 @@
 # Part I: Code Injection Attacks
 # Level 1
-# cat ctarget.l1.txt | ./hex2raw | ./ctarget -q
+cat ctarget.l1.txt | ./hex2raw | ./ctarget -q
 # Level 2
-# cat ctarget.l2.txt | ./hex2raw | ./ctarget -q
+cat ctarget.l2.txt | ./hex2raw | ./ctarget -q
 # Level 3
 cat ctarget.l3.txt | ./hex2raw | ./ctarget -q
+
+# Part II: Return-Oriented Programming
+# level 2
+cat rtarget.l2.txt | ./hex2raw | ./rtarget -q
